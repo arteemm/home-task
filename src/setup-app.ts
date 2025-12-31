238ae3075950be56260442543e55abd87a57ab69
unknown <arteemmarteemm@gmail.com>
2025-12-31 14:06:32 +0300

    add videos endpoint

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,11 +1,16 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
+import { db } from './db/in-memory.db';
+import { Video } from './videos/types/video';
  
 export const setupApp = (app: Express) => {
   app.use(express.json()); // middleware для парсинга JSON в теле запроса
  
   // основной роут
-  app.get("/", (req, res) => {
-    res.status(200).send("Hello world!");
+  app.get("/videos", (
+    req: Request,
+    res: Response<Video[] | null>,
+  ) => {
+    res.status(200).send(db.videos);
   });
   return app;
 };
