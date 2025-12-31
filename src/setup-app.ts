d91e4a3190c2604b820790067aa133febd083652
unknown <arteemmarteemm@gmail.com>
2025-12-31 19:32:44 +0300

    add delite endpoint by Id

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -18,7 +18,7 @@ export const setupApp = (app: Express) => {
     res: Response<Video | null>,
   ) => {
     const id = req.params.id;
-    const videoById = db.videos.find((k) => k.id === +id)
+    const videoById = db.videos.find((k: Video) => k.id === +id)
 
     if (!videoById) {
       res.sendStatus(404);
@@ -26,6 +26,22 @@ export const setupApp = (app: Express) => {
     }
 
     res.status(200).send(videoById);
+  });
+
+  app.delete("/videos/:id", (
+    req: Request<{ id: string }, {}, {}, {}>,
+    res: Response,
+  ) => {
+    const id = req.params.id;
+    const indexInDb = db.videos.findIndex((k: Video) => k.id === +id);
+
+    if (indexInDb < 0) {
+      res.sendStatus(404);
+      return;
+    }
+
+    db.videos.splice(indexInDb, 1);
+    res.sendStatus(204);
   });
 
   return app;
